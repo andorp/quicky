c0cc720eb6a2a25884c3c5f5a2651c2a24133ef8
andorp <andor.penzes@gmail.com>
2013-03-05 02:21:11 +0100

    ValueFrom combinator

---
+++ src/main/java/org/andorp/quicky/generator/combinator/ValueFrom.java
@@ -0,0 +1,47 @@
+package org.andorp.quicky.generator.combinator;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.andorp.quicky.Choose;
+import org.andorp.quicky.CollectionUtils;
+import org.andorp.quicky.IGenerator;
+import org.andorp.quicky.PrimitiveRandomGen;
+import org.andorp.quicky.Sanity;
+import org.andorp.quicky.engine.RandomAndSize;
+
+
+/**
+ * Randomly selects one of the given generators.
+ * 
+ * @author and.or
+ *
+ * @param <T> Type of the elements
+ */
+public final class ValueFrom<T> implements IGenerator<T> {
+    
+    private final List<IGenerator<T>> generators;
+    private final Choose<Integer> choose;
+    
+    /**
+     * @param generators Must be not null nor empty collection.
+     */
+    public ValueFrom(Collection<IGenerator<T>> generators) {
+        Sanity.notNull(generators, "OneOf: generator collection can't be null");
+        Sanity.nonEmptyCollection(generators, "OneOf: generator collection can't be empty");
+        this.generators = CollectionUtils.shallowCopyToList(generators);
+        this.choose = new Choose<Integer>(
+                PrimitiveRandomGen.integer(),
+                0,
+                (this.generators.size() - 1)
+                );
+    }
+    
+    /* Randomly uses one of the given generators
+     */
+    public T next(RandomAndSize random) {
+        final IGenerator<T> g = generators.get(choose.next(random));
+        return g.next(random);
+    }
+    
+} 
